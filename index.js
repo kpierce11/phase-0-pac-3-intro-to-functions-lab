18f5cb95e749d852bdec5aa8ccd6da2286580c46
Keira Pierce <kpierce1@samford.edu>
2024-04-08 07:35:50 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase()
+}
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+function sayHiToHeadphonedRoommate(string) {
+let cantAnswer = "I can't hear you!"
+let yesAnswer = "YES INDEED!"
+let loveAnswer = "I would love to!"
+if (string.toLowerCase() === string){
+    return cantAnswer;
+}
+else if (string.toUpperCase() === string){
+    return yesAnswer;
+}
+else if ("Let's have dinner together!"){
+    return loveAnswer;
+}
+        
+}
